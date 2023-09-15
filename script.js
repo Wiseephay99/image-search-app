1dbd776a145c2709a930c8838f1f58fdde510025
Wiseephay99 <wiseephay99@gmail.com>
2023-09-15 17:55:11 +0300

    searchapp

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const accessKey = "OyJJJGLgNrBrQGSvEXih70XYm4SEojsIVj4pwth4aL0";
+/* const accessKey = "OyJJJGLgNrBrQGSvEXih70XYm4SEojsIVj4pwth4aL0";
 
 const formEl = document.querySelector("form");
 const iputEl = document.getElementById("search-input");
@@ -52,3 +52,119 @@ formEl.addEventListener("submit", (event) =>{
  showMore.addEventListener("click", () =>{
     searchImages();
  });
+
+ 
+
+ const accessKey = "OyJJJGLgNrBrQGSvEXih70XYm4SEojsIVj4pwth4aL0";
+
+const formEl = document .querySelector ("form");
+const inputEl = document. getElementById("search-input");
+const searchResults = document. querySelector (" .search-results");
+const showMore = document. getElementById ("show-more-button" );
+
+let inputData = ""
+let page = 2;
+
+async function searchImages () 
+
+{
+    inputData = inputEl.Value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+    
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results
+    if (page === 1){
+        searchResults. innerHTML = ""
+    }
+
+    results.map((result) => {
+        const imageWrapper = document.createElement ("div");
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+
+        imageLink.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+    });
+
+    page++
+    if(page > 1){
+        showMore.style.display = "block"
+    }
+    }
+
+formEl.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    page = 1;
+    searchImages();
+ });
+ showMore.addEventListener("click", () =>{
+    searchImages();
+ });
+*/
+document.addEventListener("DOMContentLoaded", () => {
+    const accessKey = "OyJJJGLgNrBrQGSvEXih70XYm4SEojsIVj4pwth4aL0";
+
+    const formEl = document.querySelector("form");
+    const inputEl = document.getElementById("search-input");
+    const searchResults = document.querySelector(".search-results");
+    const showMore = document.getElementById("show-more-button");
+
+    let inputData = "";
+    let page = 1;
+
+    async function searchImages() {
+        inputData = inputEl.value;
+        const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const results = data.results;
+        if (page === 1) {
+            searchResults.innerHTML = "";
+        }
+
+        results.forEach((result) => {
+            const imageWrapper = document.createElement("div");
+            imageWrapper.classList.add("search-result");
+            const image = document.createElement("img");
+            image.src = result.urls.small;
+            image.alt = result.alt_description;
+            const imageLink = document.createElement("a");
+            imageLink.href = result.links.html;
+            imageLink.target = "_blank";
+            imageLink.textContent = result.alt_description;
+
+            imageLink.appendChild(image);
+            imageWrapper.appendChild(imageLink);
+            searchResults.appendChild(imageWrapper);
+        });
+
+        page++;
+        if (page > 1) {
+            showMore.style.display = "block";
+        }
+    }
+
+    // Call the searchImages function initially to fetch images when the page loads
+    searchImages();
+
+    formEl.addEventListener("submit", (event) => {
+        event.preventDefault();
+        page = 1;
+        searchImages();
+    });
+
+    showMore.addEventListener("click", () => {
+        searchImages();
+    });
+});
